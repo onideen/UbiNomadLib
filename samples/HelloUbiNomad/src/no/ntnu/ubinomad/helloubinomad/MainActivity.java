217818d3f98fded63cc35dae2e9fc1495e00f7a3
Vegar Engen <vegar.engen@gmail.com>
2014-06-05 14:32:18 +0200

    Hello ubinomad

---
+++ samples/HelloUbiNomad/src/no/ntnu/ubinomad/helloubinomad/MainActivity.java
@@ -0,0 +1,105 @@
+package no.ntnu.ubinomad.helloubinomad;
+
+import no.ntnu.ubinomad.lib.Provider;
+import no.ntnu.ubinomad.lib.activities.UbiNomadFragmentActivity;
+import no.ntnu.ubinomad.lib.externalproviders.*;
+import no.ntnu.ubinomad.lib.fragments.LoginFragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+import android.view.MenuItem;
+
+ 
+public class MainActivity extends UbiNomadFragmentActivity {
+
+
+	private static final int LOGIN = 0;
+	private static final int MAIN = 1;
+	private static final int FRAGMENT_COUNT = MAIN +1;
+
+
+	private Fragment[] fragments = new Fragment[FRAGMENT_COUNT];
+	private MenuItem settings;
+	private MenuItem main;
+
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		
+		/**
+		 * Add PlaceProviders of your choice 
+		 */
+		ProviderRegister.getInstance().addProvider(Provider.FACEBOOK, new FacebookProviders());
+		ProviderRegister.getInstance().addProvider(Provider.GOOGLE, new GoogleProvider());
+		ProviderRegister.getInstance().addProvider(Provider.FOURSQUARE, new FourSquareProvider("YOUR_CLIENT_KEY", "YOUR_SECRET_KEY"));
+
+		
+		super.onCreate(savedInstanceState);
+	
+		setContentView(R.layout.main_activity);
+		
+		FragmentManager fm = getSupportFragmentManager();
+		LoginFragment splashFragment = (LoginFragment) fm.findFragmentById(R.id.splashFragment);
+		fragments[LOGIN] = splashFragment;
+		fragments[MAIN] = fm.findFragmentById(R.id.selectionFragment);
+
+		FragmentTransaction transaction = fm.beginTransaction();
+		for(int i = 0; i < fragments.length; i++) {
+			transaction.hide(fragments[i]);
+		}
+		transaction.commit();
+		
+		showLoginFragment();
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// only add the menu when the selection fragment is showing
+		if (menu.size() == 0) {
+			main = menu.add(R.string.main);
+			settings = menu.add(R.string.settings);
+		}
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.equals(settings)) {
+			showFragment(LOGIN, true);
+			return true;
+		}
+		else if (item.equals(main)){
+			showFragment(MAIN, true);
+		}
+		
+		return false;
+	}
+
+	public void showLoginFragment() {
+		showFragment(LOGIN, true);
+	}
+
+	public void showPlacePickerFragment() {
+		showFragment(MAIN, true);
+	} 
+
+	
+	private void showFragment(int fragmentIndex, boolean addToBackStack) {
+		FragmentManager fm = getSupportFragmentManager();
+		FragmentTransaction transaction = fm.beginTransaction();
+		for (int i = 0; i < fragments.length; i++) {
+			if (i == fragmentIndex) {
+				transaction.show(fragments[i]);
+			} else {
+				transaction.hide(fragments[i]);
+			}
+		}
+		if (addToBackStack) {
+			transaction.addToBackStack(null);
+		}
+		transaction.commit();
+	}
+
+}
---
+++ samples/HelloUbiNomad/src/no/ntnu/ubinomad/helloubinomad/MainFragment.java
@@ -0,0 +1,77 @@
+package no.ntnu.ubinomad.helloubinomad;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+
+public class MainFragment extends Fragment{
+
+	protected static final String TAG = "MainFragment";
+
+	private UiLifecycleHelper uiHelper;
+	
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+		@Override
+		public void call(final Session session, final SessionState state, final Exception exception) {
+			onSessionStateChange(session, state, exception);
+		}
+	};
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		
+		View view = inflater.inflate(R.layout.main_fragment, container, false);
+		
+		return view;
+
+	}
+	
+	private void onSessionStateChange(final Session session, SessionState state, Exception exception) {
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		uiHelper = new UiLifecycleHelper(getActivity(), callback);
+		uiHelper.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		uiHelper.onResume();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle bundle) {
+		super.onSaveInstanceState(bundle);
+		uiHelper.onSaveInstanceState(bundle);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		uiHelper.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		uiHelper.onDestroy();
+	}
+
+}
